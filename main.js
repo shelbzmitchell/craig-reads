e6e2103ef2d2c4519038bab4d639c698f214c6f1
Shelby Mitchell <shelbzmitchell@gmail.com>
2025-11-30 23:00:21 -0800

    video loop

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ $(document).ready(() => {
         return false;
     });
 });
+
+var myVideo = document.getElementById('background-video');
+if (typeof myVideo.loop == 'boolean') { // loop supported
+    myVideo.loop = true;
+} else { // loop property not supported
+    myVideo.on('ended', function () {
+    this.currentTime = 0;
+    this.play();
+    }, false);
+}
+myVideo.play();
