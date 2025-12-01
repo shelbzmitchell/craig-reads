8d21623dc4136bd73347531802f0947195d79237
Shelby Mitchell <shelbzmitchell@gmail.com>
2025-11-30 23:14:32 -0800

    video for iphone

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ $(document).ready(() => {
     });
 });
 
-var myVideo = document.getElementById('background-video');
-if (typeof myVideo.loop == 'boolean') { // loop supported
-    myVideo.loop = true;
-} else { // loop property not supported
-    myVideo.on('ended', function () {
-    this.currentTime = 0;
-    this.play();
-    }, false);
-}
-myVideo.play();
+// var myVideo = document.getElementById('background-video');
+// if (typeof myVideo.loop == 'boolean') { // loop supported
+//     myVideo.loop = true;
+// } else { // loop property not supported
+//     myVideo.on('ended', function () {
+//     this.currentTime = 0;
+//     this.play();
+//     }, false);
+// }
+// myVideo.play();
